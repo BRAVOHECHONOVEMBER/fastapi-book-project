aa9f09ff85fed72402818ca543255f2d508148ca
Benjamin Ashley <119289295+BRAVOHECHONOVEMBER@users.noreply.github.com>
2025-02-14 11:07:09 +0100

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
+
 from main import app
 
-# Use base_url without the API_PREFIX
-client = TestClient(app, base_url="http://test")
+client = TestClient(app, base_url="http://test/api/v1")
